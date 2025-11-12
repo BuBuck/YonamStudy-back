6415020049cd21c2ea0b8e8ac1ea1fc088235304
BuBuck <sunjeongin123@gmail.com>
2025-11-12 18:01:01 +0900

    feat: 데이터베이스(MongoDB) 연동

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("데이터베이스(MongoDB)가 연결되었습니다.");
+    })
+    .catch((err) => {
+        console.error(err);
+    });

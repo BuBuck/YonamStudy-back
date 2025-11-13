471dd8a54c907bb79dbd6e46540ddf63f803f2f1
BuBuck <sunjeongin123@gmail.com>
2025-11-13 19:09:16 +0900

    feat: 데이터베이스 이름 설정

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,9 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 
 mongoose
-    .connect(process.env.MONGO_URI)
+    .connect(process.env.MONGO_URI, {
+        dbName: "YonamStudy",
+    })
     .then(() => {
         console.log("데이터베이스(MongoDB)가 연결되었습니다.");
     })

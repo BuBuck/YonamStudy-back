5ed5390ee1e60b78f7d13ffc8c7466e98a55a759
BuBuck <sunjeongin123@gmail.com>
2025-11-28 01:07:43 +0900

    feat: 채팅 라우터 설정 및 테스트용 코드

--- src/app.js
+++ src/app.js
@@ -15,4 +15,37 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 const authRoute = require("./routes/auth.route");
 app.use("/api/auth", authRoute);
 
+const chatRoute = require("./routes/chat.route");
+app.use("/api/chat", chatRoute);
+
+// const Group = require("./models/Group");
+// const User = require("./models/User");
+// app.get("/pGroup", async (req, res) => {
+//     const user = await User.findOne({ name: "선정인" });
+//     const myGroup = await Group.findOne({ group: "JavaScript 스터디" });
+//     user.group.pop();
+//     user.group.push(myGroup);
+//     await user.save();
+//     console.log(user);
+// });
+
+// app.get("/cGroup", async (req, res) => {
+//     Group.insertMany([
+//         {
+//             group: "JavaScript 스터디",
+//             groupMembers: [],
+//         },
+//         {
+//             group: "React 스터디",
+//             groupMembers: [],
+//         },
+//         {
+//             group: "Node.js 스터디",
+//             groupMembers: [],
+//         },
+//     ])
+//         .then(() => res.send("ok"))
+//         .catch((error) => res.send(error));
+// });
+
 module.exports = app;

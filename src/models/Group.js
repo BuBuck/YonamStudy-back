d3ccce205653f46946da68992aeaa1cca14f5538
BuBuck <sunjeongin123@gmail.com>
2025-12-08 02:23:19 +0900

    feat: hangul-js 모듈 제거

--- src/models/Group.js
+++ src/models/Group.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-const Hangul = require("hangul-js");
 
 const groupSchema = new mongoose.Schema(
     {
@@ -48,23 +47,8 @@ const groupSchema = new mongoose.Schema(
         },
 
         tags: [String],
-        searchMeta: {
-            type: String,
-            select: false,
-        },
     },
     { timestamps: true }
 );
-
-groupSchema.pre("save", function (next) {
-    // 제목과 태그를 합쳐서 검색용 문자열을 만듦
-    const rawString = `${this.title} ${this.tags.join(" ")}`;
-
-    // 한글을 자모로 분해해서 searchMeta에 저장
-    // 예: "JavaScript 스터디" -> "JavaScript ㅅㅡㅌㅓㄷㅣ"
-    this.searchMeta = Hangul.disassembleToString(rawString);
-
-    next();
-});
 
 module.exports = mongoose.model("Group", groupSchema);
